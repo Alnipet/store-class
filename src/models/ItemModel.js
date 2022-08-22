df6e75ffa6d0c9c4d969e3f33af3dc815795bfc8
Alnipet <a777petrov@gmail.com>
2022-08-22 13:17:13 +0300

    finished

---
+++ src/models/ItemModel.js
@@ -0,0 +1,9 @@
+export default class ItemModel {
+  constructor(brand, title, description, descriptionFull, currency) {
+    this.brand = brand;
+    this.title = title;
+    this.description = description;
+    this.descriptionFull = descriptionFull;
+    this.currency = currency;
+  }
+}
